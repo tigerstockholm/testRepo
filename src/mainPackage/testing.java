9a5dcdf52a37b9179aab7b9a6947a19837459d4e
tigerstockholm <fredriktigerklo@gmail.com>
2018-04-19 14:11:30 +0200

    Initial commit

---
+++ src/mainPackage/testing.java
@@ -0,0 +1,16 @@
+package mainPackage;
+
+public class testing {
+
+
+    public void testing(){
+        System.out.println();
+
+    }
+
+
+    public static void main(String[] args) {
+        testing t = new testing();
+        t.testing();
+    }
+}
